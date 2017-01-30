a32f4d11281da05305f7214b852ebd74fd0325f0
Nick Spinale <spinalen@carleton.edu>
2017-01-30 17:15:00 -0500

    remove dumb file

--- test.hs
+++
@@ -1,8 +0,0 @@
-fib :: [Integer]
-fib = 1 : 1 : zipWith (+) fib (tail fib)
-
-seive :: [Integer] -> [Integer]
-seive (p:xs) = p : seive [ x | x <- xs, x `mod` p /= 0 ]
-
-primes :: [Integer]
-primes = seive [2..]
